fa3b1640f6e8128b32f98b9d785ad742e8ad6391
Michael-Dong-awh <2501267947@qq.com>
2020-12-12 14:04:19 +0800

    DJF

--- src/com/dt95/filter/IndexFilter.java
+++ src/com/dt95/filter/IndexFilter.java
@@ -35,7 +35,7 @@ public class IndexFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
 		
-		System.out.println("进入doFILTER");
+		System.out.println("进入doFILTEJJKHJKHJKHR");
 		chain.doFilter(request, response);
 	}
 
