1a85a01a3ce2b5c4e2ce8cfa95c6721a7f68c3d3
Michael-Dong-awh <2501267947@qq.com>
2020-12-15 18:40:01 +0800

    测试

--- src/com/dt95/filter/IndexFilter.java
+++ src/com/dt95/filter/IndexFilter.java
@@ -35,7 +35,7 @@ public class IndexFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
 		
-		System.out.println("shichunfeng");
+		
 		chain.doFilter(request, response);
 	}
 
